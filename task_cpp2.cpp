834a0b5300478380992f6a4d22f1c2360efcf35b
Tanzila120 <t24461524@gmail.com>
2024-10-23 19:45:57 +0000

    task_cpp2.cpp

---
+++ task_cpp2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " 102 + 3 = " << 102 + 3 << endl;
+    cout << " 100 - 9 = " << 100 - 9 << endl;
+    cout << " 12 * 3 = " << 12 * 3 << endl;
+    cout << " 7 % 2 = " << 7 % 2 << endl;
+    cout << " 34 / 21 = " << 34 / 21 << endl;
+    cout << " 500 * 6 / 3 = " << 500 * 6 / 3 << endl;
+    cout << " 2 * (5 + 66) = " << 2 * (5 + 66) << endl;
+    return 0;
+}
